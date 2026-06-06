3c64afa1986be222f3d037db72a1491b86270695
juancarlosfc5 <181221774+juancarlosfc5@users.noreply.github.com>
2026-06-05 19:51:35 -0500

    feat: :sparkles: Add login function

--- login.py
+++ login.py
@@ -1,2 +1,12 @@
 def saludo():
     print("¡Hola! Bienvenido a mi programa de inicio de sesión.")
+
+def login():
+    username = input("Ingrese su nombre de usuario: ")
+    password = input("Ingrese su contraseña: ")
+
+    # Aquí puedes agregar la lógica para verificar el nombre de usuario y la contraseña
+    if username == "admin" and password == "1234":
+        print("¡Inicio de sesión exitoso!")
+    else:
+        print("Nombre de usuario o contraseña incorrectos. Inténtalo de nuevo.")
